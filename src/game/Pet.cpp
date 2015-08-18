6a7a67700c768d7bf8bbe09d12612aefa32a8eeb
Wolf <thomas@haahrs.com>
2015-08-18 18:08:55 +0200

    Update Pet.cpp

--- src/game/Pet.cpp
+++ src/game/Pet.cpp
@@ -747,7 +747,7 @@ void Pet::GivePetLevel(uint32 level)
 
     InitStatsForLevel(level);
     if (level >10) // only update spell for pets above level 10
-    InitLevelupSpellsForLevel();
+    	InitLevelupSpellsForLevel();
     InitTalentForLevel();
 }
 
